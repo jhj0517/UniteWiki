9e0b4376c1cb852ef37b45e234d76276a29a88c4
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-02-29 22:14:39 +0900

    update const

--- app/src/main/java/com/unitewikiapp/unitewiki/utils/Constants.kt
+++ app/src/main/java/com/unitewikiapp/unitewiki/utils/Constants.kt
@@ -1,11 +1,11 @@
 package com.unitewikiapp.unitewiki.utils
 
 object Constants {
-    const val BALANCE_RANKING = "BalancePokemonRanking"
-    const val ATTACK_RANKING = "AttackPokemonRanking"
-    const val SPEED_RANKING = "SpeedPokemonRanking"
-    const val DEFENSE_RANKING = "DefensePokemonRanking"
-    const val SUPPORT_RANKING = "SupportPokemonRanking"
+    const val BALANCE_RANKING = "balance"
+    const val ATTACK_RANKING = "attack"
+    const val SPEED_RANKING = "speed"
+    const val DEFENSE_RANKING = "defense"
+    const val SUPPORT_RANKING = "support"
     const val POKEMON_RANKS = "PokemonRanks"
     const val POKEMON_INFO = "PokemonInfo"
     const val POKEMON_REVIEW = "PokemonReviews"

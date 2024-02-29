494ddf459d6fe1db8889c11c451b445587c1ac76
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-02-29 22:25:10 +0900

    refactoring fetching info

--- app/src/main/java/com/unitewikiapp/unitewiki/viewmodels/PokemonRankViewModel.kt
+++ app/src/main/java/com/unitewikiapp/unitewiki/viewmodels/PokemonRankViewModel.kt
@@ -1,42 +1,19 @@
 package com.unitewikiapp.unitewiki.viewmodels
 
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.unitewikiapp.unitewiki.datas.PokemonRankData
 import com.unitewikiapp.unitewiki.datas.PokemonReviewsData
-import com.unitewikiapp.unitewiki.datas.RankRepository
 import com.unitewikiapp.unitewiki.datas.ReviewRepository
 import com.unitewikiapp.unitewiki.utils.Response
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonRankViewModel @Inject constructor(
-    private val repository: RankRepository,
     private val reviewRepository: ReviewRepository
     )
     : ViewModel(){
-
-    var rankList:MutableLiveData<ArrayList<PokemonRankData>> = MutableLiveData<ArrayList<PokemonRankData>>()
-
-    fun fetchRanks(type:String){
-        viewModelScope.launch {
-            val myList = arrayListOf<PokemonRankData>()
-            val res = repository.fetchRankingData(type)
-            if(res is Response.Success){
-                val data = res.data
-                myList.addAll(data)
-            } else {
-                Log.d("RankViewModel Error","Firebase Error")
-            }
-            myList.forEach { setRating(it) }
-            rankList.value = myList
-        }
-    }
 
     private suspend fun setRating(data:PokemonRankData){
         val reviewRef = reviewRepository.getReviewRef(data.pokemon_name!!)

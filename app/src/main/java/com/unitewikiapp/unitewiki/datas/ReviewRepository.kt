7d4842cb270ec320628b2d79c0dcabc7823cc424
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-02 23:36:48 +0900

    refactoring review fetching

--- app/src/main/java/com/unitewikiapp/unitewiki/datas/ReviewRepository.kt
+++ app/src/main/java/com/unitewikiapp/unitewiki/datas/ReviewRepository.kt
@@ -1,5 +1,6 @@
 package com.unitewikiapp.unitewiki.datas
 
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.Query
@@ -7,6 +8,7 @@ import com.unitewikiapp.unitewiki.di.RealTimeDataBase
 import com.unitewikiapp.unitewiki.utils.Constants
 import com.unitewikiapp.unitewiki.utils.LocaleStore
 import com.unitewikiapp.unitewiki.utils.Response
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 const val REVIEWS_NUMBER_TO_SHOW = 3
@@ -17,6 +19,15 @@ class ReviewRepository @Inject constructor(
 ): RealTimeDataBase {
 
     val locale = localeStore.findLocale()
+
+    suspend fun fetchReviewSnapshot(): Response<DataSnapshot> {
+        return try {
+            val info = rtdb.reference.child(Constants.POKEMON_REVIEW+"_"+localeStore.locale!!).get().await()
+            Response.Success(info)
+        } catch(e:Exception){
+            Response.Failure(e)
+        }
+    }
 
     suspend fun getMyReviewRef(pokemonName:String, myUid:String): Response<DatabaseReference> {
         return try{

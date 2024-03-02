cb3d5c4fd692dd331aa35d4d77368c00a3fd6cab
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-03 00:12:53 +0900

    refactoring review fetching

--- app/src/main/java/com/unitewikiapp/unitewiki/datas/PokemonInfoData.kt
+++ app/src/main/java/com/unitewikiapp/unitewiki/datas/PokemonInfoData.kt
@@ -3,7 +3,6 @@ package com.unitewikiapp.unitewiki.datas
 data class PokemonInfoData(
     val ic_pokemon: String="",
     val pokemon_name: LocaleField = LocaleField(),
-    val review_count: Long=0,
     val skills: List<Skill>?= listOf(),
     val type: String= "",
 )

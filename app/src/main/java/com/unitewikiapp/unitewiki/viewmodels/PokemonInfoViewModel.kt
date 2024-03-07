c93529e34bc7149dd4f26af5c228b9f3ef89f968
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-08 00:13:08 +0900

    remove legacy code

--- app/src/main/java/com/unitewikiapp/unitewiki/viewmodels/PokemonInfoViewModel.kt
+++ app/src/main/java/com/unitewikiapp/unitewiki/viewmodels/PokemonInfoViewModel.kt
@@ -1,16 +1,11 @@
 package com.unitewikiapp.unitewiki.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
 import com.unitewikiapp.unitewiki.datas.*
-import com.unitewikiapp.unitewiki.utils.Constants
 import com.unitewikiapp.unitewiki.utils.LocaleStore
 import com.unitewikiapp.unitewiki.utils.Response
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -30,13 +25,6 @@ class PokemonInfoViewModel @Inject constructor(
 
     private val _currentPokemon = MutableLiveData<PokemonInfoData>()
     val currentPokemon get() = _currentPokemon
-
-    val pokemonReviewData = MutableLiveData<List<PokemonReviewsData>>()
-    val averageRating = MutableLiveData<Float>(0.0f)
-    val reviewSize = MutableLiveData<Int>(0)
-    val firstRateSkillSet = MutableLiveData<Int>(0)
-    val secondRateSkillSet = MutableLiveData<Int>(0)
-    val isCalculationComplete = MutableLiveData<Boolean>(true)
 
     init {
         fetchInfoSnapshot()
@@ -80,71 +68,6 @@ class PokemonInfoViewModel @Inject constructor(
                 return
             }
         }
-    }
-
-    fun addReviewListener(pokemonName: String, query:String){
-        viewModelScope.launch {
-            val ref = reviewRepository.getQueryInfo(pokemonName,query)
-            when(ref){
-                is Response.Success -> {
-                    val valueEventListener: ValueEventListener = object : ValueEventListener {
-                        override fun onDataChange(snapshot: DataSnapshot) {
-                            val reviewList = ArrayList<PokemonReviewsData>()
-                            for (ds in snapshot.children) {
-                                val data = ds.getValue(PokemonReviewsData::class.java)!!
-                                if(auth.uid != null){
-                                    val reported:Boolean = (ds.child(Constants.QUERY_REPORTERS).child(auth.uid!!).value?:false) as Boolean
-                                    if(!reported){
-                                        reviewList.add(data)
-                                    }
-                                } else {
-                                    reviewList.add(data)
-                                }
-                                pokemonReviewData.value = reviewList.reversed()
-                            }
-                        }
-                        override fun onCancelled(error: DatabaseError) {}
-                    }
-                    ref.data.addListenerForSingleValueEvent(valueEventListener)
-                }
-                is Response.Failure ->{
-                    Log.d("InfoViewModel Query Error", "Query Error")
-                }
-            }
-        }
-    }
-
-    fun fetchRating(pokemonName: String){
-        viewModelScope.launch {
-            val ref = reviewRepository.getReviewRef(pokemonName)
-            when(ref){
-                is Response.Success -> {
-                    attachRatingListener(ref.data)
-                }
-                is Response.Failure ->{
-                    Log.d("InfoViewModel Query Error", "Query Error")
-                }
-            }
-        }
-    }
-
-    private fun attachRatingListener(ref: DatabaseReference) {
-        ref.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val reviews = snapshot.children
-                    .mapNotNull { it.getValue(PokemonReviewsData::class.java) }
-
-                reviewSize.value = reviews.size
-
-                if (reviews.isNotEmpty()) {
-                    //calculateReviewStats(reviews)
-                } else {
-                    isCalculationComplete.value = true
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {}
-        })
     }
 
 }

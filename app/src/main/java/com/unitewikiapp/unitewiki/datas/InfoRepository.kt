f929747fca191ca2fc21ab408467a1ab1c6e8650
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-08 19:26:28 +0900

    refactoring

--- app/src/main/java/com/unitewikiapp/unitewiki/datas/InfoRepository.kt
+++ app/src/main/java/com/unitewikiapp/unitewiki/datas/InfoRepository.kt
@@ -1,36 +1,22 @@
 package com.unitewikiapp.unitewiki.datas
 
 import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.unitewikiapp.unitewiki.di.RealTimeDataBase
 import com.unitewikiapp.unitewiki.utils.Constants
-import com.unitewikiapp.unitewiki.utils.LocaleStore
 import com.unitewikiapp.unitewiki.utils.Response
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class InfoRepository @Inject constructor(
-    private val rtdb: FirebaseDatabase,
-    private val localeStore: LocaleStore
+    private val rtdb: FirebaseDatabase
     ): RealTimeDataBase {
-
-    private val locale = localeStore.findLocale()
 
     suspend fun fetchInfoSnapshot(): Response<DataSnapshot> {
         return try {
             val info = rtdb.reference.child(Constants.POKEMON_INFO).get().await()
             Response.Success(info)
         } catch(e:Exception){
-            Response.Failure(e)
-        }
-    }
-
-    suspend fun getSearchDataRef(): Response<DatabaseReference> {
-        return try {
-            val ref = rtdb.reference.child(Constants.POKEMON_INFO)
-            Response.Success(ref)
-        }catch(e:Exception){
             Response.Failure(e)
         }
     }
